782810403509a86aafdd30e1342b6697d40b2f1a
Everton <123318407+Everton026@users.noreply.github.com>
2023-08-11 15:58:35 -0300

    asd

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,13 @@ import React from "react"
 
 export default function Home() {
   return (
-   <div>
-    <h1>Manipulação de Farmacia</h1>
-   </div>
+   <div className="flex-initial flex-row grid gap-5">
+    <h1 className="text-4xl font-bold">Manipulação de Farmacia</h1>
+      <p className="text-lg">   
+      Bem-vindo ao nosso site de Manipulação Farmacêutica! Aqui, unimos a ciência e a personalização para atender às suas necessidades de saúde de maneira única. <br/> Navegue por nossa ampla gama de serviços personalizados, desde formulações sob medida até orientações especializadas, tudo respaldado pela expertise dos nossos farmacêuticos. <br/> Descubra uma abordagem exclusiva para o seu bem-estar no mundo da manipulação farmacêutica
+      </p>
+      <button className="border-2 border-black rounded-md p-1">Conheça nossos Produtos!</button>
+      <button className="border-2 border-black rounded-md p-1">Pesar os Produtos!</button>
+    </div>
   )
 }
